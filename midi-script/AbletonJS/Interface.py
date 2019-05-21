3183a2439c70960c9944875693aa951f83f1d157
Leo Bernard <admin@leolabs.org>
2019-05-21 21:06:12 +0200

    :sparkles: Implement nsid mechanism to supply data to a namespace

--- midi-script/AbletonJS/Interface.py
+++ midi-script/AbletonJS/Interface.py
@@ -3,16 +3,20 @@ class Interface(object):
         self.ableton = c_instance
         self.socket = socket
         self.log_message = log
-        self.ns = None
+
+    def get_ns(self, nsid):
+        pass
 
     def handle(self, payload):
         func = getattr(self, payload["name"])
         uuid = payload["uuid"] if payload.has_key("uuid") else None
+        ns = self.get_ns(nsid=payload["nsid"]
+                         if payload.has_key("nsid") else None)
 
         if func is not None and callable(func):
             try:
                 result = func(
-                    **payload["args"]) if payload.has_key("args") else func()
+                    ns=ns, **payload["args"]) if payload.has_key("args") else func(ns=ns)
                 self.socket.send("result", result, uuid)
             except Exception, e:
                 self.socket.send("error", str(e.args), uuid)
@@ -20,37 +24,37 @@ class Interface(object):
             self.socket.send(
                 "error", "Function call failed: " + payload["name"] + " doesn't exist or isn't callable", uuid)
 
-    def add_listener(self, prop, eventId):
+    def add_listener(self, ns, prop, eventId):
         try:
-            add_fn = getattr(self.ns, "add_" + prop + "_listener")
+            add_fn = getattr(ns, "add_" + prop + "_listener")
         except:
             raise Exception("Listener " + str(prop) + " does not exist.")
 
         def fn():
-            return self.socket.send(eventId, self.get_prop(prop))
+            return self.socket.send(eventId, self.get_prop(ns, prop))
 
         add_fn(fn)
         return eventId
 
-    def remove_listener(self, prop):
+    def remove_listener(self, ns, prop):
         try:
-            remove_fn = getattr(self.ns, "remove_" + prop + "_listener")
+            remove_fn = getattr(ns, "remove_" + prop + "_listener")
             return remove_fn()
         except:
             raise Exception("Listener " + str(prop) + " does not exist.")
 
-    def get_prop(self, prop):
+    def get_prop(self, ns, prop):
         try:
             get_fn = getattr(self, "get_" + prop)
         except:
-            def get_fn(): return getattr(self.ns, prop)
+            def get_fn(ns): return getattr(ns, prop)
 
-        return get_fn()
+        return get_fn(ns)
 
-    def set_prop(self, prop, value):
+    def set_prop(self, ns, prop, value):
         try:
             set_fn = getattr(self, "set_" + prop)
         except:
-            def set_fn(): return setattr(self.ns, prop, value)
+            def set_fn(ns, value): return setattr(ns, prop, value)
 
-        return set_fn(value)
+        return set_fn(ns, value)
--- midi-script/AbletonJS/Song.py
+++ midi-script/AbletonJS/Song.py
@@ -4,16 +4,24 @@ from Interface import Interface
 class Song(Interface):
     def __init__(self, c_instance, socket, log):
         super(Song, self).__init__(c_instance, socket, log)
-        self.ns = self.ableton.song()
 
-    def get_cue_points(self):
+    def get_ns(self, nsid):
+        return self.ableton.song()
+
+    def get_cue_points(self, ns):
         cues = []
-        for cue in self.ns.cue_points:
+        for cue in ns.cue_points:
             cues.append({"name": cue.name, "time": cue.time})
         return cues
 
-    def jump_to_cue(self, time):
-        for cue in self.ns.cue_points:
+    def get_clip_trigger_quantization(self, ns):
+        return str(ns.clip_trigger_quantization)
+
+    def get_midi_recording_quantization(self, ns):
+        return str(ns.midi_recording_quantization)
+
+    def jump_to_cue(self, ns, time):
+        for cue in ns.cue_points:
             if cue.time == time:
                 cue.jump()
                 return True
