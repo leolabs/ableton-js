b2221b2bf9f0968f52d5e9320574f45ff8a19d8c
Leo Bernard <admin@leolabs.org>
2019-05-22 20:04:52 +0200

    :sparkles: Implement further getters for Song

--- midi-script/AbletonJS/Song.py
+++ midi-script/AbletonJS/Song.py
@@ -1,4 +1,7 @@
 from Interface import Interface
+from CuePoint import CuePoint
+from Scene import Scene
+from Track import Track
 
 
 class Song(Interface):
@@ -9,16 +12,28 @@ class Song(Interface):
         return self.ableton.song()
 
     def get_cue_points(self, ns):
-        cues = []
-        for cue in ns.cue_points:
-            cues.append({"name": cue.name, "time": cue.time})
-        return cues
+        return map(CuePoint.serialize_cue_point, ns.cue_points)
 
     def get_clip_trigger_quantization(self, ns):
         return str(ns.clip_trigger_quantization)
 
     def get_midi_recording_quantization(self, ns):
         return str(ns.midi_recording_quantization)
+
+    def get_master_track(self, ns):
+        return Track.serialize_track(ns.master_track)
+
+    def get_return_tracks(self, ns):
+        return map(Track.serialize_track, ns.return_tracks)
+
+    def get_scenes(self, ns):
+        return map(Scene.serialize_scene, ns.scenes)
+
+    def get_tracks(self, ns):
+        return map(Track.serialize_track, ns.tracks)
+
+    def get_visible_tracks(self, ns):
+        return map(Track.serialize_track, ns.visible_tracks)
 
     def jump_to_cue(self, ns, time):
         for cue in ns.cue_points:
