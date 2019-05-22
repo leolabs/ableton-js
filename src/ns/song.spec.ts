8bc8dbfcb5415605ba7b0769be1754c1fb65fa6b
Leo Bernard <admin@leolabs.org>
2019-05-22 22:19:57 +0200

    :sparkles: Add Jest for testing

---
+++ src/ns/song.spec.ts
@@ -0,0 +1,61 @@
+import { Ableton } from "../index";
+import { GettableProperties } from "./song";
+
+const a = new Ableton();
+
+const gettableProps: (keyof GettableProperties)[] = [
+  "arrangement_overdub",
+  "back_to_arranger",
+  "can_capture_midi",
+  "can_jump_to_next_cue",
+  "can_jump_to_prev_cue",
+  "can_redo",
+  "can_undo",
+  "clip_trigger_quantization",
+  "count_in_duration",
+  "cue_points",
+  "current_song_time",
+  "exclusive_arm",
+  "exclusive_solo",
+  "groove_amount",
+  "is_counting_in",
+  "is_playing",
+  "last_event_time",
+  "loop",
+  "loop_length",
+  "loop_start",
+  "master_track",
+  "metronome",
+  "midi_recording_quantization",
+  "nudge_down",
+  "nudge_up",
+  "overdub",
+  "punch_in",
+  "punch_out",
+  "re_enable_automation_enabled",
+  "record_mode",
+  "return_tracks",
+  "root_note",
+  "scale_name",
+  "scenes",
+  "select_on_launch",
+  "session_automation_record",
+  "session_record",
+  "session_record_status",
+  "signature_denominator",
+  "signature_numerator",
+  "song_length",
+  "swing_amount",
+  "tempo",
+  "tracks",
+  "view",
+  "visible_tracks",
+];
+
+describe("Song", () => {
+  it("should be able to read all properties without erroring", async () => {
+    await Promise.all(gettableProps.map(p => a.song.get(p)));
+  });
+});
+
+afterAll(() => a.close());
