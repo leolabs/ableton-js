04ea28d5cc18b06018168626db778d7cfe542685
Leo Bernard <admin@leolabs.org>
2019-05-29 19:49:39 +0200

    :sparkles: Add SongView

--- midi-script/AbletonJS.py
+++ midi-script/AbletonJS.py
@@ -6,6 +6,7 @@ from Device import Device
 from DeviceParameter import DeviceParameter
 from Scene import Scene
 from Song import Song
+from SongView import SongView
 from Track import Track
 from _Framework.ControlSurface import ControlSurface
 
@@ -24,6 +25,7 @@ class AbletonJS(ControlSurface):
             "device-parameter": DeviceParameter(c_instance, self.socket),
             "scene": Scene(c_instance, self.socket),
             "song": Song(c_instance, self.socket),
+            "song-view": SongView(c_instance, self.socket),
             "track": Track(c_instance, self.socket),
         }
 
---
+++ midi-script/SongView.py
@@ -0,0 +1,24 @@
+from Interface import Interface
+from DeviceParameter import DeviceParameter
+from Scene import Scene
+from Track import Track
+
+
+class SongView(Interface):
+    def __init__(self, c_instance, socket):
+        super(SongView, self).__init__(c_instance, socket)
+
+    def get_ns(self, nsid):
+        return self.ableton.song().view
+
+    def select_device(self, ns, device_id):
+        return ns.select_device(Interface.get_obj(device_id))
+
+    def get_selected_parameter(self, ns):
+        return DeviceParameter.serialize_device_paramater(ns.selected_parameter)
+
+    def get_selected_track(self, ns):
+        return Track.serialize_track(ns.selected_track)
+
+    def get_selected_scene(self, ns):
+        return Scene.serialize_scene(ns.selected_scene)
