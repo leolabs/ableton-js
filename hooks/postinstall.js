f7e6403fcb3d4488a511d9925eb916b166195485
Leo Bernard <admin@leolabs.org>
2020-03-05 03:21:37 +0100

    :lipstick: Make post-install warning message yellow

--- hooks/postinstall.js
+++ hooks/postinstall.js
@@ -1,3 +1,3 @@
 console.warn(
-  "🚨 Ableton.js changed the default ports from 9000 and 9001 to 9004 and 9005. Please update the Python plugin as well.",
+  "\x1b[33m🚨 Ableton.js changed the default ports from 9000 and 9001 to 9004 and 9005. Please update the Python plugin as well.\x1b[0m",
 );
