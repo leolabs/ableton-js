c47e423360c7ef5a783235c8311e874a8c9f8eb3
Leo Bernard <leo@leolabs.org>
2022-03-26 13:16:38 +0100

    :construction: Try moving the thread to a separate class

--- midi-script/Socket.py
+++ midi-script/Socket.py
@@ -1,9 +1,9 @@
 import socket
 import json
 import struct
-import threading
 import zlib
-from threading import Timer
+from threading import Timer, Thread
+from timeit import default_timer as timer
 
 
 def split_by_n(seq, n):
@@ -11,6 +11,60 @@ def split_by_n(seq, n):
     while seq:
         yield seq[:n]
         seq = seq[n:]
+
+
+class ThreadSocket(Thread):
+    def __init__(self, host, port, handler, log_message, show_message):
+        super(ThreadSocket, self).__init__()
+        self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.log_message = log_message
+        self.show_message = show_message
+        self.host = host
+        self.port = port
+        self.input_handler = handler
+
+    def bind(self):
+        debug_addr = str(self.host) + ":" + str(self.port)
+
+        try:
+            self.log_message("Starting socket on " + debug_addr)
+            self._socket.bind((self.host, self.port))
+        except:
+            msg = 'ERROR: Cannot bind to ' + debug_addr + ', port in use. Trying again...'
+            self.show_message(msg)
+            self.log_message(msg)
+            t = Timer(5, self.bind)
+            t.start()
+
+    def run(self):
+        self.bind()
+        buffer = bytes()
+
+        while 1:
+            try:
+                data = self._socket.recv(65536)
+                if len(data) and self.input_handler:
+                    buffer += data[1:]
+
+                    # \xFF for Live 10 (Python2) and 255 for Live 11 (Python3)
+                    if(data[0] == b'\xFF' or data[0] == 255):
+                        start = timer()
+                        unzipped = zlib.decompress(buffer)
+                        payload = json.loads(unzipped)
+
+                        self.log_message("Receiving: " + str(payload))
+                        self.input_handler(payload)
+                        end = timer()
+                        self.log_message("Received in " +
+                                         str(end - start) + "ms")
+                        buffer = bytes()
+            except socket.error as e:
+                if e.errno == 35:
+                    continue
+                else:
+                    raise e
+            except Exception as e:
+                self.log_message("Error while processing: " + str(e.args))
 
 
 class Socket(object):
@@ -24,28 +78,10 @@ class Socket(object):
         Socket.show_message = func
 
     def __init__(self, handler, remotehost='127.0.0.1', remoteport=9031, localhost='127.0.0.1', localport=9041):
-        self.input_handler = handler
-
         self._socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        # self._socket.setblocking(0)
-
-        self._local_addr = (localhost, localport)
         self._remote_addr = (remotehost, remoteport)
-
-        self.bind()
-
-    def bind(self):
-        try:
-            self._socket.bind(self._local_addr)
-            self.log_message('Starting on: ' + str(self._local_addr) +
-                             ', remote addr: ' + str(self._remote_addr))
-        except:
-            msg = 'ERROR: Cannot bind to ' + \
-                str(self._local_addr) + ', port in use. Trying again...'
-            self.show_message(msg)
-            self.log_message(msg)
-            t = Timer(5, self.bind)
-            t.start()
+        self.socket_thread = ThreadSocket(
+            localhost, localport, handler, self.log_message, self.show_message)
 
     def _sendto(self, msg):
         '''Send a raw message to the client, compressed and chunked, if necessary'''
@@ -80,31 +116,5 @@ class Socket(object):
     def shutdown(self):
         self._socket.close()
 
-    def process(self):
-        self.log_message("Starting socket thread")
-        try:
-            buffer = bytes()
-            while 1:
-                try:
-                    data = self._socket.recv(65536)
-                    if len(data) and self.input_handler:
-                        buffer += data[1:]
-
-                        # \xFF for Live 10 (Python2) and 255 for Live 11 (Python3)
-                        if(data[0] == b'\xFF' or data[0] == 255):
-                            unzipped = zlib.decompress(buffer)
-                            payload = json.loads(unzipped)
-
-                            self.log_message("Receiving: " + str(payload))
-                            self.input_handler(payload)
-                            buffer = bytes()
-                except socket.error as e:
-                    if e.errno == 35:
-                        continue
-                    else:
-                        raise e
-        except Exception as e:
-            self.log_message("Error while processing: " + str(e.args))
-
     def start_thread(self):
-        threading.Thread(target=self.process, daemon=True).start()
+        self.socket_thread.start()
